8d025344c8571f46d0ae6223a498d7b8d2855cc3
Cody Swendrowski <cswendrowski@mail.com>
2013-03-27 21:40:57 -0500

    Sets up basis for connecting and hosting in Pong Example.

--- src/petri/api/Examples/PongExample/Host.java
+++ src/petri/api/Examples/PongExample/Host.java
@@ -1,5 +1,9 @@
 package petri.api.Examples.PongExample;
 
+import java.awt.Graphics;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import petri.api.GameEngine;
 import petri.api.GameMode;
 
@@ -8,12 +12,25 @@ import petri.api.GameMode;
  *
  */
 public class Host extends GameMode {
-
+	
 	/**
 	 * @param eng
 	 */
 	public Host(GameEngine eng) {
 		super(eng);
+		try {
+			eng.networkAdapter.host(42);
+		} catch (IOException e) {
+			GameEngine.log(e.getMessage());
+		}
+		InternetGame.setOutput(eng.networkAdapter.getOutputStream());
 	}
 
+	
+	@Override
+	public void paint(Graphics g){
+		g.setColor(Color.black);
+		g.fillRect(0, 0, engine.getEnvironmentSize().x, engine.getEnvironmentSize().y);
+		g.drawString("Currently hosting from ", 0, 0);
+	}
 }
--- src/petri/api/Examples/PongExample/InternetGame.java
+++ src/petri/api/Examples/PongExample/InternetGame.java
@@ -1,6 +1,7 @@
 package petri.api.Examples.PongExample;
 
 import java.awt.event.KeyEvent;
+import java.io.DataOutputStream;
 
 import petri.api.GameEngine;
 import petri.api.GameMode;
@@ -28,5 +29,13 @@ public class InternetGame extends LocalGame {
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		super.keyPressed(e);
+	}
+
+	/**
+	 * @param outputStream
+	 */
+	public static void setOutput(DataOutputStream outputStream) {
+		// TODO Auto-generated method stub
+		
 	}
 }
