749c2e6fe49e2d9bb317ae11651f0fdef3622885
Cody Swendrowski <bob324@msn.com>
2013-03-31 10:34:13 -0500

    Adds in objects to control for InternetGame in Pong Example.

--- src/petri/api/Examples/PongExample/InternetGame.java
+++ src/petri/api/Examples/PongExample/InternetGame.java
@@ -1,8 +1,13 @@
 package petri.api.Examples.PongExample;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.io.DataOutputStream;
+import java.io.IOException;
 
+import petri.api.DualResolutionGameImage;
 import petri.api.GameEngine;
 import petri.api.GameMode;
 
@@ -12,11 +17,47 @@ import petri.api.GameMode;
  */
 public class InternetGame extends GameMode {
 
+	private boolean up, down, isHost;
+	private Paddle left, right;
+	private int leftScore, rightScore;
+	private Font scoreFont = new Font("Monospaced", Font.PLAIN, 30);
+	private DualResolutionGameImage ballImage = null;
+	private Ball ball;
+	
 	/**
 	 * @param eng
 	 */
 	public InternetGame(GameEngine eng) {
 		super(eng);
+		down = false;
+		up = false;
+		isHost = false;
+		
+		try {
+			ballImage = new DualResolutionGameImage(
+					"Examples/PongExample/Resources/", "ball_high.png",
+					"ball_low.png");
+		} catch (IOException e) {
+			GameEngine.log(this.toString() + ": " + e.getMessage());
+		}
+		ball = new Ball(engine, ballImage);
+		ball.setCenter((engine.getEnvironmentSize().x / 2)
+				- (ball.getSize().x / 2), (engine.getEnvironmentSize().y / 2)
+				- (ball.getSize().y / 2));
+
+		left = new Paddle(engine);
+		right = new Paddle(engine);
+
+		left.setCenter(10, (engine.getEnvironmentSize().y / 2) - 15);
+		right.setCenter(engine.getEnvironmentSize().x - right.getSize().x - 15,
+				(engine.getEnvironmentSize().y / 2) - 15);
+
+		engine.actors.add(ball);
+		engine.actors.add(left);
+		engine.actors.add(right);
+
+		leftScore = 0;
+		rightScore = 0;
 	}
 	
 	@Override
@@ -24,18 +65,48 @@ public class InternetGame extends GameMode {
 		// TODO Auto-generated method stub
 		super.run(ms);
 	}
+	@Override
+	public void paint(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(0, 0, engine.getEnvironmentSize().x,
+				engine.getEnvironmentSize().y);
+		engine.actors.drawActors(g);
+		g.setColor(Color.green);
+		g.setFont(scoreFont);
+		engine.centerTextHorizontally(g,
+				leftScore + "     :     " + rightScore, 0,
+				engine.getEnvironmentSize().x, 30);
+		super.paint(g);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+			up = false;
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			down = false;
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		super.keyPressed(e);
+
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+			up = true;
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			down = true;
+
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
+			engine.setCurrentGameMode("MainMenu");
 	}
 
-	/**
-	 * @param outputStream
-	 */
-	public static void setOutput(DataOutputStream outputStream) {
-		// TODO Auto-generated method stub
-		
+	public void scorePoint(String side) {
+		if (side.equalsIgnoreCase("left"))
+			leftScore++;
+		else if (side.equalsIgnoreCase("right"))
+			rightScore++;
+	}
+	
+	public void setHost(boolean host) {
+		isHost = host;
 	}
 }
